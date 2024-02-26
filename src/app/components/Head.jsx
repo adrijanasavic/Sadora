71fd1bbe7c9c0f97b35e76bbdf7edfd51216b6e7
adrijanasavic <adrijana.work@gmail.com>
2024-02-26 15:05:01 +0100

    Head

---
+++ src/app/components/Head.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Head() {
+  return (
+    <div>
+      <div className="bg-red-300 py-2 ">
+      <p className="text-white text-sm md:text-base text-center">
+        Dobrodošli na stranicu najlepših ručnih radova
+      </p>
+    </div>
+    </div>
+  )
+}
